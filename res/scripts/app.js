fd5152a39ba8f62f22fa0cebceed5a8d7831f92c
Emil <emil93@gmail.com>
2019-03-08 23:24:58 +0100

    small tweaks

--- res/scripts/app.js
+++ res/scripts/app.js
@@ -1,13 +1,13 @@
 function initListeners() {
   document.querySelector('#welcome .subfooter i').addEventListener('click', () => {
-    scrollToPage('about');
-  });
-  document.querySelector('#about .subfooter i').addEventListener('click', () => {
     scrollToPage('skills');
-  })
-  document.querySelector('#skills .subfooter i').addEventListener('click', () => {
-    scrollToPage('welcome');
   });
+  document.querySelector('#skills .subfooter i').addEventListener('click', () => {
+    scrollToPage('contact');
+  });
+  document.querySelector('#contact .subfooter i').addEventListener('click', () => {
+    scrollToPage('welcome');
+  })
 }
 
 function scrollToPage(name) {
